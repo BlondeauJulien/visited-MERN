cd197ad70fa057a78bf9c2fdf049294e8b77f548
Julien Blondeau <julienblondeau@live.fr>
2020-01-05 23:20:54 +0100

    add useParams for dynamic routing and display

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { userParams, useParams } from 'react-router-dom'
 
 import PlaceList from '../components/PlaceList';
 
-const UserPlaces = () => {
 const DUMMY_PLACES = [
     {
         id: 'p1',
@@ -20,7 +20,7 @@ const DUMMY_PLACES = [
         id: 'p2',
         title: 'Empire State Building',
         description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/71/33/e6.jpg',
+        imageUrl: 'https://www.voyageway.com/wp-content/uploads/2018/04/empire-state-building-ou-top-of-the-rock-740x447.jpg',
         address: '20 W 34th St, New York, NT, 10001',
         location: {
             lat: 40.7484405,
@@ -30,8 +30,12 @@ const DUMMY_PLACES = [
     }
 ]
 
+const UserPlaces = () => {
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+
     return (
-        <PlaceList items={DUMMY_PLACES}/>
+        <PlaceList items={loadedPlaces}/>
     )
 }
 
