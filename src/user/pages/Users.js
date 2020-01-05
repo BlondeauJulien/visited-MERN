440238c721eb7d1247507f27b9291bca706aba9e
Julien Blondeau <julienblondeau@live.fr>
2020-01-05 17:07:51 +0100

    create usersList and UserItem components with dummy content

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
-    return (
-        <h2>
-            Users Works!
-        </h2>
-    )
+    const USERS = [{id: 'u1', name: 'Julien', image:`https://www.ef.com/wwen/blog/wp-content/uploads/2018/05/Dublin_is_the_Silicon_Valley_of_Europe-and_the_hot_new_place_to_study_English_blog-hero_low.jpg`, places: 3}]
+
+    return <UsersList items={USERS}/>
 }
 
 export default Users
