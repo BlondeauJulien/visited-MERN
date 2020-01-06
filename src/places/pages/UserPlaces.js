aa76c9b01020da3cd63ae4a9cc028d39738af038
Julien Blondeau <julienblondeau@live.fr>
2020-01-06 19:30:03 +0100

    add interactive map on modal with openStreetMap

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { userParams, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import PlaceList from '../components/PlaceList';
 
