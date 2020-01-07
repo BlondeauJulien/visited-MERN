c27ee5f3cb47553192540b24ac48b634e87758c6
Julien Blondeau <julienblondeau@live.fr>
2020-01-07 15:31:20 +0100

    handle form submition

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -53,8 +53,13 @@ const NewPlace = () => {
         })
     }, []);
 
+    const placeAddHandler = e => {
+        e.preventDefault();
+        console.log(formState.inputs);
+    }
+
     return (
-        <form className='place-form'>
+        <form className='place-form' onSubmit={placeAddHandler}>
             <Input 
                 id="title"
                 element="input" 
@@ -70,6 +75,14 @@ const NewPlace = () => {
                 label="Description" 
                 validators={[VALIDATOR_MINLENGTH(5)]} 
                 errorText="Please enter a valid description (at least 5 characters)."
+                onInput={inputHandler}
+            />
+            <Input 
+                id="address"
+                element="input" 
+                label="Address" 
+                validators={[VALIDATOR_REQUIRE()]} 
+                errorText="Please enter a valid address."
                 onInput={inputHandler}
             />
             <Button type="submit" disabled={!formState.isValid} >
