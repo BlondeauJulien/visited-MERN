6a78840ed9fdf874d40a56be4ac9e8cc6c7fc2d5
Julien Blondeau <julienblondeau@live.fr>
2020-01-12 22:32:04 +0100

    configure multer in the backend

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const usersControllers = require('../controllers/users-controller');
+const fileUpload = require('../middleware/file.upload');
 
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
 
 router.post(
-    '/signup', 
+    '/signup',
+    fileUpload.single('image'), //we expect an image key that should hold our image
     [
         check('name').not().isEmpty(),
         check('email').normalizeEmail().isEmail(),
